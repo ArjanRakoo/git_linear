bec3581fc56145228e266f5283f55efc288a1b71
Arjan <arjan@rakoo.com>
2026-02-23 15:03:39 +0100

    extend argument options and remove current branch from deletion list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from controller import Controller
+import argparse
 
 controller = Controller()
 
@@ -11,6 +12,17 @@ optionsMap = {
 }
 
 
+def build_parser():
+    parser = argparse.ArgumentParser(description="Linear + Git helper CLI")
+
+    parser.add_argument("--switch-branch", "--swb", action="store_true", help="Switch Branch")
+    parser.add_argument("--delete-branch", "--delb", action="store_true", help="Delete Branch")
+    parser.add_argument("--create-branch-from-issue", "--crb", action="store_true", help="Create Branch from Issue")
+    parser.add_argument("--list-review-requests", "--rr", action="store_true", help="List Review Requests")
+
+    return parser
+
+
 def get_main_options():
     options = []
 
@@ -20,8 +32,35 @@ def get_main_options():
     return options
 
 
+def handle_args():
+    args = build_parser().parse_args()
+
+    if args.switch_branch:
+        controller.switch_branch()
+        return True
+
+    if args.delete_branch:
+        controller.delete_local_branch()
+        return True
+
+    if args.create_branch_from_issue:
+        controller.create_branch_from_issue()
+        return True
+
+    if args.list_review_requests:
+        controller.list_review_requests()
+        return True
+
+    return False
+
+
 def run():
     if not controller.check_repo():
+        return
+
+    should_exit = handle_args()
+
+    if should_exit:
         return
 
     while True:
--- utils/git/branches.py
+++ utils/git/branches.py
@@ -33,25 +33,32 @@ class Branches:
 
 
     def delete_local_branch(self):
-        disabled = {"master", "develop"}
-
-        local_branches = self.get_local_branches()
+        disabled = {"master", "main", "develop"}
 
         options = []
 
-        for branch in local_branches:
-            if branch not in disabled:
+        for branch in self.get_local_branches():
+            if branch not in disabled and branch != self.get_current_branch():
                 options.append(branch)
 
         answer = select.prompt_for_choice(options, multi=True)
         
         if (answer == None):
-            print("Was cancelled")
             return
 
         for branch in answer:
             subprocess.run(["git", "branch", "-D", branch], cwd=self.cwd)
             print(f"Branch {branch} deleted")
+
+    def get_current_branch(self):
+        result = subprocess.run(
+            ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+            cwd=self.cwd,
+            capture_output=True,
+            text=True,
+        )
+
+        return result.stdout.strip()
 
 
 
